e476dda388009a9f6a0a5e0c0a20632184941f99
Rohit Goyal <rohitgoyal4220@gmail.com>
2025-12-31 00:44:33 +0530

    Updated API base URL to live cloud endpoint

--- client/utils/api.ts
+++ client/utils/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+  // OLD: baseURL: 'http://localhost:5000/api',
+  // NEW (Live Cloud URL):
+  baseURL: 'https://inventory-management-system-ipdw.onrender.com/api', 
 });
 
 export default api;
